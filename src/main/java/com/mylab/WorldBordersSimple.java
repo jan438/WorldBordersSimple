3ba1639a0252b0b5daaabd9ae97499c2d42b302b
Jan Boon <janboon438@gmail.com>
2015-07-13 09:38:49 +0200

    Netherlands borders extracted in a separate file

--- src/main/java/com/mylab/WorldBordersSimple.java
+++ src/main/java/com/mylab/WorldBordersSimple.java
@@ -3,6 +3,7 @@ package com.mylab;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -27,6 +28,7 @@ public class WorldBordersSimple {
 	public static void main(String[] args) {
 
 		try {
+			PrintWriter writer = new PrintWriter("netherlands_borders.txt", "UTF-8");
 			InputStream inputstream = new FileInputStream(
 					"/home/jan/Downloads/TM_WORLD_SIMPLE/TM_WORLD_SIMPLE.json");
 			FeatureCollection featureCollection = new FeatureCollection();
@@ -46,59 +48,64 @@ public class WorldBordersSimple {
 				geometry = (LinkedHashMap<String, Object>) feature
 						.getGeometry();
 				System.out.println("FCount: " + feature_count + " : " + name);
-				type = (String) geometry.get("type");
-				System.out.println("Geometry type: " + type);
-				if (type.equals("MultiPolygon")) {
-					coordinates = (ArrayList<String>) geometry
-							.get("coordinates");
-					System.out.println("Number of polygons: "
-							+ coordinates.size());
-					Iterator<String> poly_it = coordinates.iterator();
-					int poly_count = 0;
-					while (poly_it.hasNext()) {
-						Object polygon = poly_it.next();
-						ArrayList<String> latlng = (ArrayList<String>) polygon;
-						Iterator<String> po = latlng.iterator();
-						int pcount = 0;
-						while (po.hasNext()) {
-							Object s = po.next();
-							ArrayList<String> pp = (ArrayList<String>) s;
-							Iterator<String> kk = pp.iterator();
-							while (kk.hasNext()) {
-								Object point = kk.next();
-								System.out.println("Poly: " + poly_count
-										+ " Point: " + point);
+				if (name.equals("Netherlands")) {
+					type = (String) geometry.get("type");
+					System.out.println("Geometry type: " + type);
+					if (type.equals("MultiPolygon")) {
+						coordinates = (ArrayList<String>) geometry
+								.get("coordinates");
+						System.out.println("Number of polygons: "
+								+ coordinates.size());
+						Iterator<String> poly_it = coordinates.iterator();
+						int poly_count = 0;
+						while (poly_it.hasNext()) {
+							Object polygon = poly_it.next();
+							ArrayList<String> latlng = (ArrayList<String>) polygon;
+							Iterator<String> po = latlng.iterator();
+							int pcount = 0;
+							while (po.hasNext()) {
+								Object s = po.next();
+								ArrayList<String> pp = (ArrayList<String>) s;
+								Iterator<String> kk = pp.iterator();
+								while (kk.hasNext()) {
+									Object point = kk.next();
+									System.out.println("Poly: " + poly_count
+											+ " Point: " + point);
+									writer.println(point);
+									pcount++;
+								}
+							}
+							poly_count++;
+						}
+					}
+					if (type.equals("Polygon")) {
+						coordinates = (ArrayList<String>) geometry
+								.get("coordinates");
+						System.out.println("Number of polygons: "
+								+ coordinates.size());
+						Iterator<String> cit = coordinates.iterator();
+						int ccount = 0;
+						while (cit.hasNext()) {
+							Object coordinate = cit.next();
+							ArrayList<String> latlng = (ArrayList<String>) coordinate;
+							Iterator<String> po = latlng.iterator();
+							int pcount = 0;
+							while (po.hasNext()) {
+								Object s = po.next();
+								ArrayList<String> pp = (ArrayList<String>) s;
+								System.out.println("Point: " + s);
+								writer.println(s);
 								pcount++;
 							}
+							ccount++;
 						}
-						poly_count++;
+						System.out.println("Total coordinates: " + ccount);
 					}
-				}
-				if (type.equals("Polygon")) {
-					coordinates = (ArrayList<String>) geometry
-							.get("coordinates");
-					System.out.println("Number of polygons: "
-							+ coordinates.size());
-					Iterator<String> cit = coordinates.iterator();
-					int ccount = 0;
-					while (cit.hasNext()) {
-						Object coordinate = cit.next();
-						ArrayList<String> latlng = (ArrayList<String>) coordinate;
-						Iterator<String> po = latlng.iterator();
-						int pcount = 0;
-						while (po.hasNext()) {
-							Object s = po.next();
-							ArrayList<String> pp = (ArrayList<String>) s;
-							System.out.println("Point: " + s);
-							pcount++;
-						}
-						ccount++;
-					}
-					System.out.println("Total coordinates: " + ccount);
 				}
 				feature_count++;
 				/* if (feature_count==2) break; */
 			}
+			writer.close();
 			System.out.println("Total features: " + feature_count);
 		} catch (JsonGenerationException e) {
 			e.printStackTrace();
